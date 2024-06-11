4e36074395a35b751b9fc92b93704b61bfce5f7d
Vitor <reis.consulo@gmail.com>
2024-06-11 18:17:00 -0300

    Criando elementos

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-Input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const CancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+
+// Função
+
+// Eventos
